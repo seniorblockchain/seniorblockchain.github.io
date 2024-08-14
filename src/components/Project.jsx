d4436509eb6169abae86b2c1e9f6d9b766190eb7
Milad Raeisi <miladsoft@yahoo.com>
2024-08-14 13:35:10 +0400

    Create project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,31 +1,31 @@
 import { motion } from "framer-motion";
 
-import blog1 from "../assets/images/blog1.png";
-import blog2 from "../assets/images/blog2.png";
-import blog3 from "../assets/images/blog3.png";
+import project1 from "../assets/images/project1.png";
+import project2 from "../assets/images/project2.png";
+import project3 from "../assets/images/project3.png";
 
-const blogData = [
+const  projectData = [
   {
     title: "AI and Machine Learning",
     subtitle:
       "Discover insights and trends in the world of data analytics. Delve into how AI and machine learning are revolutionizing industries",
-    image: blog1.src,
+    image: project1.src,
   },
   {
     title: "Data-Driven Analytics",
     subtitle: "Explore the impact of AI and ML on data analytics.",
-    image: blog2.src,
+    image: project2.src,
   },
   {
     title: "Real-Time Data Processing",
     subtitle: "Learn about the importance of processing real-time.",
-    image: blog3.src,
+    image: project3.src,
   },
 ];
 
-export const Blog = () => (
+export const Project = () => (
   <section className="w-screen flex justify-center bg-bgDark2 relative ">
-    <div className="absolute -top-16" id="blog" />
+    <div className="absolute -top-16" id="project" />
     <div className="pb-0 pt-4 bg-bgDark2 2xl:w-[1200px] lg:w-[1000px] xl:w-[1150px]  ">
       <motion.div
         initial={{ opacity: 0 }}
@@ -35,7 +35,7 @@ export const Blog = () => (
       >
         <div className="container px-4 mb-20">
           <div className="max-w-2xl text-center lg:text-left mb-16 mx-auto lg:ml-0 lg:mr-0">
-            <span className="block-subtitle">Our Blog</span>
+            <span className="block-subtitle">Our Project</span>
             <h2 className="mt-6 mb-6 text-4xl lg:text-5xl font-bold font-heading text-primaryText">
               Latest Insights
             </h2>
@@ -45,27 +45,27 @@ export const Blog = () => (
           </div>
           <div className="flex 2xl:w-[1200px] lg:w-[1000px] xl:w-[1150px] flex-wrap -mx-4 items-start h-auto sm:h-[30rem] lg:h-[31rem]  xl:h-[35rem]">
             <div className="flex sm:hidden lg:flex w-11/12 mx-auto sm:ml-0 sm:mr-0 lg:w-1/2 xl:w-3/5 px-4 mb-8 lg:mb-0  h-full">
-              <a href="/blog/article">
+              <a href=" /project/article">
                 <div className="p-6 sm:p-10 bg-bgDark3 rounded-3xl h-full hover:bg-bgDark3Hover transition cursor-pointer">
                   <img
-                    src={blogData[0].image}
-                    alt={blogData[0].title}
+                    src={ projectData[0].image}
+                    alt={ projectData[0].title}
                     className="rounded-3xl mb-6  w-full"
-                    aria-label={blogData[0].title}
+                    aria-label={ projectData[0].title}
                   />
                   <h3 className="mb-4 text-2xl font-bold font-heading text-primaryText">
-                    {blogData[0].title}
+                    { projectData[0].title}
                   </h3>
                   <p className="text-secondaryText leading-loose">
-                    {blogData[0].subtitle}
+                    { projectData[0].subtitle}
                   </p>
                 </div>
               </a>
             </div>
             <div className="hidden sm:flex mx-auto lg:ml-0 lg:mr-0 w-11/12 sm:w-4/5 lg:w-1/2 xl:w-2/5 px-4  flex-col justify-between h-full">
-              {blogData.slice(1).map((post, index) => (
+              { projectData.slice(1).map((post, index) => (
                 <a
-                  href="/blog/article"
+                  href=" /project/article"
                   key={`${post.title}-${index}`}
                   className=" flex  gap-4 p-10 bg-bgDark3 rounded-3xl  min-h-1/2 h-1/2 max-h-[47%]
                   hover:bg-bgDark3Hover transition cursor-pointer"
